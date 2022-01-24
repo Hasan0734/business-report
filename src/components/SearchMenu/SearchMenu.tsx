ab2c7dc7859d291954dc0954ff174b414bb1efcf
Jahid Hasan <jahidhasan2856@gmail.com>
2022-01-24 15:24:28 +0600

    align fix

--- src/components/SearchMenu/SearchMenu.tsx
+++ src/components/SearchMenu/SearchMenu.tsx
@@ -3,9 +3,9 @@
 const SearchMenu = ({ data }) => {
     return (
         <>
-            <div className="flex justify-between my-6 flex-col sm:flex-row">
-                <h1 className="text-2xl font-semibold text-center">{data.title}</h1>
-                <div className="flex gap-2 justify-center  mt-5 sm:mt-0">
+            <div className="flex justify-between items-center my-6 flex-col sm:flex-row">
+                <h1 className="text-2xl font-semibold text-center mb-2 sm:mb-0">{data.title}</h1>
+                <div className="flex gap-2 justify-center ">
                     <input className='p-1 border border-orange w-80 focus:outline-none' type="text" />
                     <button type="submit" className="text-white px-3  rounded-sm bg-orange">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" /></svg>
